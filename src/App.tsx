b2be8f39ae08e4b505c393309e3b220247aa3123
vid.zganjar7@gmail.com <vid.zganjar7@gmail.com>
2024-06-18 14:45:03 +0200

    template

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import Navbar from './pages/navbar'
+import Domov from "./pages/domov"
+import About from "./pages/about"
+import Trgovina from "./pages/trgovina"
+import Novice from "./pages/novice"
+import Footer from "./pages/footer"
+import { Route, Routes} from 'react-router-dom'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar />
+      <Routes>
+        <Route path="/home" element={<Domov />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/trgovina" element={<Trgovina />} />
+        <Route path="/novice" element={<Novice />} />
+      </Routes>
+    <Footer />
+    </>
+  )
+}
+
+export default App
