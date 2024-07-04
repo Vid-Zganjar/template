1a01538cbe1e426b8babb82a2a97a16ddd699b1c
vid.zganjar7@gmail.com <vid.zganjar7@gmail.com>
2024-07-04 11:20:50 +0200

    navbar trgovina

--- src/pages/trgovina.tsx
+++ src/pages/trgovina.tsx
@@ -15,84 +15,369 @@
 const products = [
   {
     id: 1,
-    name: 'Nomad Pouch',
-    href: '/majica',
-    price: '$50',
-    availability: 'White and Black',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-07-product-01.jpg',
-    imageAlt: 'White fabric pouch with white zipper, black zipper pull, and black elastic loop.',
+    name: "Nomad Pouch",
+    href: "/majica",
+    price: "$50",
+    availability: "White and Black",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-07-product-01.jpg",
+    imageAlt:
+      "White fabric pouch with white zipper, black zipper pull, and black elastic loop.",
   },
   {
     id: 2,
-    name: 'Zip Tote Basket',
-    href: '#',
-    price: '$140',
-    availability: 'Washed Black',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-07-product-02.jpg',
-    imageAlt: 'Front of tote bag with washed black canvas body, black straps, and tan leather handles and accents.',
+    name: "Zip Tote Basket",
+    href: "#",
+    price: "$140",
+    availability: "Washed Black",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-07-product-02.jpg",
+    imageAlt:
+      "Front of tote bag with washed black canvas body, black straps, and tan leather handles and accents.",
   },
   {
     id: 3,
-    name: 'Medium Stuff Satchel',
-    href: '#',
-    price: '$220',
-    availability: 'Blue',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-07-product-03.jpg',
+    name: "Medium Stuff Satchel",
+    href: "#",
+    price: "$220",
+    availability: "Blue",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-07-product-03.jpg",
     imageAlt:
-      'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+      "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
   },
   {
     id: 4,
-    name: 'Nomad Pouch',
-    href: '/šalce',
-    price: '$50',
-    availability: 'White and Black',
-    imageSrc: 'https://www.forlifedesignusa.com/cdn/shop/products/521-CAR.jpg?v=1582155776',
-    imageAlt: 'White fabric pouch with white zipper, black zipper pull, and black elastic loop.',
+    name: "Nomad Pouch",
+    href: "/šalce",
+    price: "$50",
+    availability: "White and Black",
+    imageSrc:
+      "https://www.forlifedesignusa.com/cdn/shop/products/521-CAR.jpg?v=1582155776",
+    imageAlt:
+      "White fabric pouch with white zipper, black zipper pull, and black elastic loop.",
   },
   {
     id: 5,
-    name: 'Zip Tote Basket',
-    href: '#',
-    price: '$140',
-    availability: 'Washed Black',
-    imageSrc: 'https://eu.keepcup.com/media/wysiwyg/Homepage_2023/DYO.gif',
-    imageAlt: 'Front of tote bag with washed black canvas body, black straps, and tan leather handles and accents.',
+    name: "Zip Tote Basket",
+    href: "#",
+    price: "$140",
+    availability: "Washed Black",
+    imageSrc: "https://eu.keepcup.com/media/wysiwyg/Homepage_2023/DYO.gif",
+    imageAlt:
+      "Front of tote bag with washed black canvas body, black straps, and tan leather handles and accents.",
   },
   {
     id: 6,
-    name: 'Medium Stuff Satchel',
-    href: '#',
-    price: '$220',
-    availability: 'Blue',
-    imageSrc: 'https://m.media-amazon.com/images/I/41m9XdsXEqL._AC_UF894,1000_QL80_.jpg',
+    name: "Medium Stuff Satchel",
+    href: "#",
+    price: "$220",
+    availability: "Blue",
+    imageSrc:
+      "https://m.media-amazon.com/images/I/41m9XdsXEqL._AC_UF894,1000_QL80_.jpg",
     imageAlt:
-      'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+      "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
   },
   // More products...
-]
+];
+import { useState } from "react";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+ 
+  TransitionChild,
+} from "@headlessui/react";
+import {
+  Bars3Icon,
+  CalendarIcon,
+  ChartPieIcon,
+  DocumentDuplicateIcon,
+  FolderIcon,
+  HomeIcon,
+  UsersIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
 
+const navigation = [
+  { name: "NOVO", href: "#", icon: HomeIcon, current: true },
+  { name: "Moški", href: "#", icon: UsersIcon, current: false },
+  { name: "Ženske", href: "#", icon: FolderIcon, current: false },
+  { name: "Otroci", href: "#", icon: CalendarIcon, current: false },
+  { name: "Dodatki", href: "#", icon: DocumentDuplicateIcon, current: false },
+  { name: "Aktualno", href: "#", icon: ChartPieIcon, current: false },
+];
+const teams = [
+  { id: 1, name: "Majce", href: "#", initial: "H", current: false },
+  { id: 2, name: "Šalčke", href: "#", initial: "T", current: false },
+  { id: 3, name: "Obeski", href: "#", initial: "W", current: false },
+];
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 export default function Example() {
+  /*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-7xl overflow-hidden px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8">
-          {products.map((product) => (
-            <a key={product.id} href={product.href} className="group text-sm">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center"
-                />
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-white">
+        <body class="h-full">
+        ```
+      */}
+      <div>
+        <Dialog
+          className="relative z-50 lg:hidden"
+          open={sidebarOpen}
+          onClose={setSidebarOpen}
+        >
+          <DialogBackdrop
+            transition
+            className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
+          />
+
+          <div className="fixed inset-0 flex">
+            <DialogPanel
+              transition
+              className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full"
+            >
+              <TransitionChild>
+                <div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
+                  <button
+                    type="button"
+                    className="-m-2.5 p-2.5"
+                    onClick={() => setSidebarOpen(false)}
+                  >
+                    <span className="sr-only">Close sidebar</span>
+                    <XMarkIcon
+                      className="h-6 w-6 text-white"
+                      aria-hidden="true"
+                    />
+                  </button>
+                </div>
+              </TransitionChild>
+              {/* Sidebar component, swap this element with another sidebar if you like */}
+              <div className="mt-16 flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
+                <nav className="flex flex-1 flex-col">
+                  <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                    <li>
+                      <ul role="list" className="-mx-2 space-y-1">
+                        {navigation.map((item) => (
+                          <li key={item.name}>
+                            <a
+                              href={item.href}
+                              className={classNames(
+                                item.current
+                                  ? "bg-gray-50 text-indigo-600"
+                                  : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
+                                "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+                              )}
+                            >
+                              <item.icon
+                                className={classNames(
+                                  item.current
+                                    ? "text-indigo-600"
+                                    : "text-gray-400 group-hover:text-indigo-600",
+                                  "h-6 w-6 shrink-0"
+                                )}
+                                aria-hidden="true"
+                              />
+                              {item.name}
+                            </a>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                    <li>
+                      <div className="text-xs font-semibold leading-6 text-gray-400">
+                        Your teams
+                      </div>
+                      <ul role="list" className="-mx-2 mt-2 space-y-1">
+                        {teams.map((team) => (
+                          <li key={team.name}>
+                            <a
+                              href={team.href}
+                              className={classNames(
+                                team.current
+                                  ? "bg-gray-50 text-indigo-600"
+                                  : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
+                                "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+                              )}
+                            >
+                              <span
+                                className={classNames(
+                                  team.current
+                                    ? "border-indigo-600 text-indigo-600"
+                                    : "border-gray-200 text-gray-400 group-hover:border-indigo-600 group-hover:text-indigo-600",
+                                  "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border bg-white text-[0.625rem] font-medium"
+                                )}
+                              >
+                                {team.initial}
+                              </span>
+                              <span className="truncate">{team.name}</span>
+                            </a>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                    <li className="mt-auto">
+                      <a
+                        href="#"
+                        className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600"
+                      ></a>
+                    </li>
+                  </ul>
+                </nav>
               </div>
-              <h3 className="mt-4 font-medium text-gray-900">{product.name}</h3>
-              <p className="italic text-gray-500">{product.availability}</p>
-              <p className="mt-2 font-medium text-gray-900">{product.price}</p>
-            </a>
-          ))}
+            </DialogPanel>
+          </div>
+        </Dialog>
+
+        {/* Static sidebar for desktop */}
+        <div className="mt-32 hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+          {/* Sidebar component, swap this element with another sidebar if you like */}
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
+            <div className="flex h-16 shrink-0 items-center"></div>
+            <nav className="flex flex-1 flex-col">
+              <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                <li>
+                  <ul role="list" className="-mx-2 space-y-1">
+                    {navigation.map((item) => (
+                      <li key={item.name}>
+                        <a
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? "bg-gray-50 text-indigo-600"
+                              : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
+                            "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+                          )}
+                        >
+                          <item.icon
+                            className={classNames(
+                              item.current
+                                ? "text-indigo-600"
+                                : "text-gray-400 group-hover:text-indigo-600",
+                              "h-6 w-6 shrink-0"
+                            )}
+                            aria-hidden="true"
+                          />
+                          {item.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+                <li>
+                  <div className="text-xs font-semibold leading-6 text-gray-400">
+                    Tisk po meri
+                  </div>
+                  <ul role="list" className="-mx-2 mt-2 space-y-1">
+                    {teams.map((team) => (
+                      <li key={team.name}>
+                        <a
+                          href={team.href}
+                          className={classNames(
+                            team.current
+                              ? "bg-gray-50 text-indigo-600"
+                              : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
+                            "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+                          )}
+                        >
+                          <span
+                            className={classNames(
+                              team.current
+                                ? "border-indigo-600 text-indigo-600"
+                                : "border-gray-200 text-gray-400 group-hover:border-indigo-600 group-hover:text-indigo-600",
+                              "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border bg-white text-[0.625rem] font-medium"
+                            )}
+                          >
+                            {team.initial}
+                          </span>
+                          <span className="truncate">{team.name}</span>
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+        <div className="lg:pl-72">
+          <div className="sticky top-0 z-40 lg:mx-auto lg:max-w-7xl lg:px-8">
+            <div className="flex h-16 md:h-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-0 lg:shadow-none">
+              <button
+                type="button"
+                className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+                onClick={() => setSidebarOpen(true)}
+              >
+                <span className="sr-only">Open sidebar</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </button>
+
+              {/* Separator */}
+              
+
+              
+            </div>
+          </div>
+
+          <main className="py-10">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <div className="bg-white">
+                <div className="mx-auto max-w-7xl overflow-hidden px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+                  <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8">
+                    {products.map((product) => (
+                      <a
+                        key={product.id}
+                        href={product.href}
+                        className="group text-sm"
+                      >
+                        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                          <img
+                            src={product.imageSrc}
+                            alt={product.imageAlt}
+                            className="h-full w-full object-cover object-center"
+                          />
+                        </div>
+                        <h3 className="mt-4 font-medium text-gray-900">
+                          {product.name}
+                        </h3>
+                        <p className="italic text-gray-500">
+                          {product.availability}
+                        </p>
+                        <p className="mt-2 font-medium text-gray-900">
+                          {product.price}
+                        </p>
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
         </div>
       </div>
-    </div>
-  )
+    </>
+  );
 }
